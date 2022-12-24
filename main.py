bedd7c3d8d49638f69b36b8b228b30e3a1dc2c85
Amin Saedi <amin.di.sam@gmail.com>
2022-12-24 00:11:43 -0500

    basic ui added

--- main.py
+++ main.py
@@ -1,17 +1,77 @@
 import time
 
 import pytermgui as ptg
+from pytermgui.enums import SizePolicy as SP
+
+default_encoder = "[2 @34;123;255]Cipher text is: [inverse]"
+default_decoder = "[2 @34;123;255]Decoded text is: [inverse]"
+
+encoder_text_input = ptg.InputField(prompt="Enter Plain Text: ")
+encoder_result = ptg.Label()
+decoder_result = ptg.Label()
+decoder_text_input = ptg.InputField(prompt="Enter Encoded Text: ")
 
 
-def macro_time(fmt: str) -> str:
-    return time.strftime(fmt)
+def handle_encode(e):
+    val = encoder_text_input.value
+    if len(val):
+        encoder_result.value = ""
+        return
+    encoder_result.value = default_encoder + val
 
 
-ptg.tim.define("!time", macro_time)
+def handle_decode(e):
+    val = decoder_text_input.value
+    if len(val):
+        decoder_result.value = ""
+        return
+    decoder_result.value = default_decoder + val
+
+
+encoder_button = ptg.Button("Encode Value", onclick=handle_encode)
+decoder_button = ptg.Button("Decode Value", onclick=handle_decode)
+
+
+def _define_layout():
+    layout = ptg.Layout()
+    layout.add_slot("Header", height=1)
+    layout.add_break()
+    layout.add_slot("body_top")
+    layout.add_break()
+    layout.add_slot("body_bottom")
+    layout.add_break()
+    layout.add_slot("footer", height=1)
+
+    return layout
 
 
 with ptg.WindowManager() as manager:
-    manager.layout.add_slot("Body")
-    manager.add(
-        ptg.Window("[bold]The current time is:[/]\n\n[!time 25]%c[/]\n\n[78 @34;123;255]My name is: [inverse] Amin [/]", box="EMPTY")
-    )
+    manager.layout = _define_layout()
+
+    manager.add(ptg.Window("[bold]Enigma Python App", box="EMPTY"))
+
+    encoder = ptg.Window(
+                   encoder_text_input,
+                   "\n\n",
+                   encoder_button,
+                   "\n\n",
+                   encoder_result,
+                   title="Encoder",
+            )
+
+    decoder = ptg.Window(
+                    decoder_text_input,
+                    "\n\n",
+                    decoder_button,
+                    "\n\n",
+                    decoder_result,
+                    title="Decoder"
+            )
+
+    manager.add(encoder, assign="body_top")
+    manager.add(decoder, assign="body_bottom")
+
+    footer = ptg.Window(ptg.Button("Quit"), box="EMPTY")
+
+    manager.add(footer, assign="footer")
+
